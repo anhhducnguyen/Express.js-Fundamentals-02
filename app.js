baba38e78552e033a0a81f09ff2391e3b7b4333b
DucAnh <anhnguyen2k373@gmail.com>
2025-02-19 10:01:37 +0700

    Bài 2: Routing Nâng Cao trong Express.js

--- app.js
+++ app.js
@@ -3,10 +3,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// Bài 2: Routing Nâng Cao trong Express.js
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('This is home page');
+})
+
+app.get('/ask', (req, res) => {
+    res.send('This is asking page');
+})
+
+app.get('/question-detail/:id', (req, res) => {
+    res.send('This is a question detail page');
+})
+
+app.use((req, res) => {
+    res.status(404).send('Page Not Found');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
